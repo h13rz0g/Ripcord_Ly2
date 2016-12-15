62999d8f81d67abb53026738e5695101de8c0922
h13rz0g <MichelleS.un@outlook.com>
2016-12-15 14:29:33 +0800

    Ripcord_Ly2.Tool

---
+++ src/Ripcord_Ly2.Tool/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ripcord_Ly2.Tool
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
